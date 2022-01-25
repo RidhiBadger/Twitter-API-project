ac4a23d00abe050ca25f15fab1e008096dcad630
Ridhi Gupta <rgupta84@wisc.edu>
2022-01-24 21:23:43 -0600

    Read the tweets from API and write them into Database, working in SQL in Python

---
+++ server_postgres.py
@@ -0,0 +1,121 @@
+import psycopg2
+import twitter
+import json
+import argparse
+import spacy
+import re
+from datetime import datetime
+import time
+import re
+import requests
+import os
+
+
+api = twitter.Api(consumer_key = 'J4QehTb6WzpQSUMStM778oI5r', consumer_secret = 'C3MRCzY45H3pDy5rklPlE8aqon1ubxhVfT0gU7BEgCeJhYXgHY', access_token_key = '928600292408438785-MzCzpbk9FzynYLc5PYP3LPEs3tqxJT2', access_token_secret = 'jFndq7Bz3O5uGTLz5GHJqjq8JmUTOul7KyNurKYwwKpgS')
+
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+
+regex = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
+
+
+#establishing the connection
+connection = psycopg2.connect(
+   dbname="twitteranalysis", user='gb760', password='gb760'
+)
+connection.autocommit = True
+
+#Creating a cursor object using the cursor() method
+cursor = connection.cursor()
+
+#Preparing query to create a database
+#sql = '''CREATE database mydb''';
+
+#Creating a database
+#cursor.execute(sql)
+#print("Database created successfully........")
+
+
+
+
+
+
+
+
+
+
+
+
+
+#connection = psycopg2.connect("dbname=twitteranalysis user=gb760")
+#cursor = connection.cursor()
+
+def clean_text(text):
+   if type(text) != str:
+       text = text.decode("utf-8")
+   doc = re.sub(regex, '', text, flags=re.MULTILINE) # remove URLs
+   sentences = []
+   for sentence in doc.split("\n"):
+       if len(sentence) == 0:
+           continue
+       sentences.append(sentence)
+   doc = nlp("\n".join(sentences))
+   doc = " ".join([token.lemma_.lower().strip() for token in doc
+                       if (not token.is_stop)
+                           and (not token.like_url)
+                           and (not token.lemma_ == "-PRON-")
+                           and (not len(token) < 4)])
+   return doc
+
+ 
+tweets = api.GetStreamSample()
+
+
+for tweet in tweets:
+  if 'delete' in tweet.keys():
+      continue    
+  if tweet['lang'] == "en":
+     tweet['text'] = clean_text(tweet['text'])
+     date = datetime.strptime(tweet['created_at'],"%a %b %d %H:%M:%S +0000 %Y")
+     date1 = date.strftime("%Y-%m-%d %H:%M:00")   #Removing the seconds from date-time
+
+     #postgres_query_insert = """INSERT INTO tweets_table (timestamp, tweet_text) VALUES ( %s, %s)"""
+     #records = (date1, tweet['text'])
+     #cursor.execute(postgres_query_insert, records)
+     #connection.commit()
+     words = tweet['text'].split()
+     for i in range(len(words)):
+        tweetword = words[i]
+        print(tweetword)   #to check if the code is working fine, can be removed otherwise
+        #print(words)   #to check if the code is working fine
+        #print(len(words))  #to check if the code is working fine
+        
+        postgres_query_insert2 = """ INSERT INTO words (time, words) VALUES (%s, %s)"""
+        record_to_insert = (date1, tweetword)
+        cursor.execute(postgres_query_insert2, record_to_insert)
+        connection.commit()       
+    
+
+	
+
+
+
+
+
+
+
+       
+#Closing the connection
+connection.close()      
+       
+
+       
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
